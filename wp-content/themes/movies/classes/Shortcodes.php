6b14b20f743576ca346c966b01dcf179679f56a9
Ihor <ihor.prachuk@gmail.com>
2021-01-02 16:00:39 +0200

    feat: add new shortcodes
    -add shortcode for archive movies
    -add shortcode for single movie

--- wp-content/themes/movies/classes/Shortcodes.php
+++ wp-content/themes/movies/classes/Shortcodes.php
@@ -1,27 +1,57 @@
 <?php
 class Shortcodes
 {
-    //Шорткод для вывода фильмов
-    private function wp_show_movies_shortcode()
+
+    public function init()
+    {
+        add_shortcode('show_movies_archive_shortcode', [$this, 'wp_show_movies_archive_shortcode']);
+
+        add_shortcode('show_single_movie_shortcode', [$this, 'wp_show_single_movie_shortcode']);
+    }
+
+    public function wp_show_movies_archive_shortcode()
     {
         while(have_posts())
         {
             the_post();
-            the_title("<h2>", "</h2>");
-            the_content();
+            the_title("<h2 class='post_title'><a href=".get_permalink().">", "</a></h2>");
             wp_link_pages();
+            $metaboxes = get_post_custom(get_the_id());
+            print_r("<img src={$metaboxes['_poster'][0]} alt='No poster'>");
             ?>
             
             <?php
         }
     }
-    public function init()
+
+    public function wp_show_single_movie_shortcode()
     {
-        add_shortcode('show_movies_shortcode', function() {
-            $this->wp_show_movies_shortcode();
-        });
+        while(have_posts())
+        {
+            the_post();
+            the_title("<h2 class='post_title'><a href=".get_permalink().">", "</a></h2>");
+            wp_link_pages();
+            $metaboxes = get_post_custom(get_the_id());
+            print_r("<img src={$metaboxes['_poster'][0]} alt='No poster'>");
+            the_content();
+            foreach($metaboxes as $key => $value)
+            {
+                if($key[0] == '_')
+                {
+                    continue;
+                }else
+                {
+                    $key = ucfirst(str_replace('_', ' ', $key));
+                }
+                ?>
+                <p><strong><?=$key?></strong>: <?=$value[0]?></p>
+                <?php
+            }
+            ?>
+            
+            <?php
+        }
     }
 
-    
 }
 ?>
