92443f95d1dee93fed989f6e861d0b8d58efcf0e
Ihor <ihor.prachuk@gmail.com>
2021-01-04 17:07:39 +0200

    feat: add pagination

--- wp-content/themes/movies/archive-movies.php
+++ wp-content/themes/movies/archive-movies.php
@@ -1,9 +1,9 @@
 <?php get_header() ?>
-<span class='posts_pagination'><?php the_posts_pagination($args = array(
+<span class='posts_pagination'> <?php the_posts_pagination($args = array(
     'screen_reader_text' => __('Pages'),
-)) ?></span>
+)) ?> </span>
 <?php do_shortcode('[show_movies_archive_shortcode]') ?>
-<span class='posts_pagination'><?php the_posts_pagination($args = array(
+<span class='posts_pagination'> <?php the_posts_pagination($args = array(
     'screen_reader_text' => __('Pages'),
-)) ?></span>
+)) ?> </span>
 <?php get_footer() ?>
