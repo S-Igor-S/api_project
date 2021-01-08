a48de0d81c8edb0c8771c670f48a060fc58f43fc
S-Igor-S <72615778+S-Igor-S@users.noreply.github.com>
2021-01-08 13:52:25 +0200

    fix: change position of some code

---
+++ wp-content/themes/movies/page-send-movie.php
@@ -0,0 +1,17 @@
+<?php require CLASSES_PATH."SendMovies.php" ?>
+<?php ob_start() ?>
+<?php get_header() ?>
+<?php
+    
+    do_shortcode('[add_movies_shortcode]');
+    
+    if(!empty($_REQUEST))
+    {
+        $apiRequest = new ApiRequest($_REQUEST);
+        $apiRequest->send_request();
+        header("Location: ".$_SERVER['REQUEST_URI']);
+    }
+
+?>
+<?php get_footer() ?>
+<?php ob_end_flush() ?>
