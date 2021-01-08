4a06a02f428ba0eede49fd0f344adfd87f1b9c03
S-Igor-S <72615778+S-Igor-S@users.noreply.github.com>
2021-01-08 10:56:20 +0200

    Delete archive-movies.php

--- movies/archive-movies.php
+++
@@ -1,9 +0,0 @@
-<?php get_header() ?>
-<span class='posts_pagination'> <?php the_posts_pagination($args = array(
-    'screen_reader_text' => __('Pages'),
-)) ?> </span>
-<?php do_shortcode('[show_movies_archive_shortcode]') ?>
-<span class='posts_pagination'> <?php the_posts_pagination($args = array(
-    'screen_reader_text' => __('Pages'),
-)) ?> </span>
-<?php get_footer() ?>
