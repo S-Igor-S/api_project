6d7518328d51b8a5d0e006ade2b8ffbbde69f606
S-Igor-S <72615778+S-Igor-S@users.noreply.github.com>
2021-01-08 13:55:20 +0200

    feat: connect new classes
    -add page for api credits
    -registration nav menu

--- wp-content/themes/movies/functions.php
+++ wp-content/themes/movies/functions.php
@@ -1,30 +1,59 @@
 <?php
-//Подключаем классы
 
-require_once 'classes/Styles.php';
-require_once 'classes/PostMovies.php';
-require_once 'classes/AddMovies.php';
+//Config file
 
-require_once 'classes/Shortcodes.php';
+require_once 'config.php';
 
-//Подключение класса стилей
+//Classes
+
+require_once CLASSES_PATH.'Styles.php';
+require_once CLASSES_PATH.'PostMovies.php';
+require_once CLASSES_PATH.'Shortcodes.php';
+require_once CLASSES_PATH.'AddMetaBoxes.php';
+require_once CLASSES_PATH.'CronProcesses.php';
+require_once CLASSES_PATH.'ApiCredits.php';
+
+//CSS && JS
 
 $style = new Styles;
 $style->init();
 
-//Создание post type
+//Create post type
 
 $postMovie = new PostMovies;
 $postMovie->init();
 
-//Добавляем шорткоды
+//Shortcodes
 
 $shortcode = new Shortcodes;
 $shortcode->init();
 
-//Добавляем фильмы
-//Подключать один раз
+//Meta boxes
 
-// $addMovies = new AddMovies;
-// $addMovies->add_movies();
+new AddMetaBoxes($id = 'genres', $title = 'Genres', $screen = 'movies');
+new AddMetaBoxes($id = 'release_date', $title = 'Release date', $screen = 'movies');
+new AddMetaBoxes($id = 'budget', $title = 'Budget', $screen = 'movies');
+new AddMetaBoxes($id = 'revenue', $title = 'Revenue', $screen = 'movies');
+new AddMetaBoxes($id = 'runtime', $title = 'Runtime', $screen = 'movies');
+new AddMetaBoxes($id = 'production_countries', $title = 'Production countries', $screen = 'movies');
+
+//Add cron processes (add movies)
+
+$cronProcesses = new CronProcesses;
+$cronProcesses->activate_cron();
+
+//Add option page
+
+$apiCredits = new ApiCredits;
+$apiCredits->init();
+
+
+add_action( 'init', 'register_my_menus' );
+function register_my_menus() {
+    register_nav_menus(
+        array(
+            'nav-menu' => __( 'Nav Menu' ),
+        )
+    );
+}
 ?>
