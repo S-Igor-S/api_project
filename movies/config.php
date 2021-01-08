6afbb2d75d3c08842a0fa2f86ee9d494989176fa
S-Igor-S <72615778+S-Igor-S@users.noreply.github.com>
2021-01-08 10:56:27 +0200

    Delete config.php

--- movies/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-ini_set ('max_execution_time', '300');
-define("FILE_PATH", dirname(__FILE__));
-define("CLASSES_PATH", FILE_PATH."/classes/");
-define("TEMPLATES_PATH", FILE_PATH. "/templates/");
-define("VENDOR_PATH", FILE_PATH. "/vendor/");
-?>
