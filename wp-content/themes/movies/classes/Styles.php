de2006eb1ceb257e333e47bc0ba6bf282dd77241
S-Igor-S <72615778+S-Igor-S@users.noreply.github.com>
2021-01-08 13:45:35 +0200

    feat: connect new styles

--- wp-content/themes/movies/classes/Styles.php
+++ wp-content/themes/movies/classes/Styles.php
@@ -3,17 +3,26 @@
 class Styles 
 {
 
-    private function load_styles()
-    {
-        //$ver = filemtime( get_template_directory().'/assets/css/style.css';
-        return wp_enqueue_style($handle = 'custom-css', $src = get_template_directory_uri().'/assets/css/style.css');
-    }
-
     public function init()
     {
-        add_action('wp_enqueue_scripts', function () {
-            $this->load_styles();
-        });
+        add_action('wp_enqueue_scripts', [$this, 'load_styles']);
+    }
+
+    public function load_styles()
+    {
+        wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-1.11.0.min.js');
+        wp_enqueue_script('jquery-migrate', 'https://code.jquery.com/jquery-migrate-1.2.1.min.js');
+
+        wp_enqueue_style($handle = 'style', $src = get_template_directory_uri() . '/style.css');
+
+        wp_enqueue_style($handle = 'custom-single-movie-css', $src = get_template_directory_uri().'/assets/css/single_movie_styles.css');
+        wp_enqueue_style($handle = 'custom-archive-movies-css', $src = get_template_directory_uri().'/assets/css/archive_movies_styles.css');
+        wp_enqueue_style($handle = 'custom-archive-movies-css', $src = get_template_directory_uri().'/assets/css/styles.css');
+
+        wp_enqueue_style('slick-css', get_template_directory_uri().'/assets/slick/slick.css');
+        wp_enqueue_style('slick-theme', get_template_directory_uri().'/assets/slick/slick-theme.css');
+    
+        wp_enqueue_script('slick-js', get_template_directory_uri().'/assets/slick/slick.js');
     }
 
 }
