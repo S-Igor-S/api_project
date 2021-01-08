5ecd9462cab58a6b7e8d41975bcde305531f10ec
Ihor <ihor.prachuk@gmail.com>
2021-01-08 09:10:48 +0200

    fix: change cron works

---
+++ wp-content/themes/movies/classes/CronProcesses.php
@@ -0,0 +1,30 @@
+<?php
+// use classes\DownloadMovies;
+
+require_once CLASSES_PATH.'DownloadMovies.php';
+require_once VENDOR_PATH.'Kama_Cron.php';
+
+class CronProcesses
+{
+    public function activate_cron()
+    {
+        new Kama_Cron([
+            'id' => 'cron_processes',
+            'events' => array(
+                'cron_add_movies' => array(
+                    'callback' => [$this, 'cron_add_movies'],
+                    'start_time' => time() + 30,
+                    'interval_name' => 'every_day',
+                    'interval_sec' =>HOUR_IN_SECONDS * 8,
+                    'interval_desc' => 'Каждый день',
+                ),
+            ),
+        ]);
+    }
+    public function cron_add_movies()
+    {
+        $addMovies = new DownloadMovies;
+        $addMovies->add_movies();
+    }
+}
+?>
